e709e849a01198ce6d24cef6435e21d09e0601f3
MCullers <cullers14@gmail.com>
2019-04-05 01:53:45 -0500

    friend finder update

---
+++ app/data/friends.js
@@ -0,0 +1,102 @@
+var friends = [
+    {
+      name: "Joey Tribbiani",
+      photo: "https://upload.wikimedia.org/wikipedia/en/d/da/Matt_LeBlanc_as_Joey_Tribbiani.jpg",
+      scores: [
+        "4",
+        "4",
+        "4",
+        "1",
+        "5",
+        "1",
+        "2",
+        "4",
+        "5",
+        "1"
+      ]
+    },
+    {
+      name: "Chandler Bing",
+      photo: "https://upload.wikimedia.org/wikipedia/en/6/6c/Matthew_Perry_as_Chandler_Bing.jpg",
+      scores: [
+        "2",
+        "3",
+        "3",
+        "3",
+        "4",
+        "2",
+        "5",
+        "3",
+        "5",
+        "3"
+      ]
+    },
+    {
+      name: "Rachel Green",
+      photo: "https://upload.wikimedia.org/wikipedia/en/e/ec/Jennifer_Aniston_as_Rachel_Green.jpg",
+      scores: [
+        "2",
+        "1",
+        "2",
+        "1",
+        "3",
+        "3",
+        "3",
+        "3",
+        "3",
+        "2"
+      ]
+    },
+    {
+      name: "Monica Geller",
+      photo: "https://upload.wikimedia.org/wikipedia/en/d/d0/Courteney_Cox_as_Monica_Geller.jpg",
+      scores: [
+        "2",
+        "5",
+        "4",
+        "3",
+        "1",
+        "5",
+        "3",
+        "3",
+        "3",
+        "4"
+      ]
+    },
+    {
+      name: "Phoebe Buffay",
+      photo: "https://upload.wikimedia.org/wikipedia/en/f/f6/Friendsphoebe.jpg",
+      scores: [
+        "3",
+        "2",
+        "5",
+        "5",
+        "3",
+        "1",
+        "2",
+        "5",
+        "3",
+        "3"
+      ]
+    },
+    {
+      name: "Ross Geller",
+      photo: "https://upload.wikimedia.org/wikipedia/en/6/6f/David_Schwimmer_as_Ross_Geller.jpg",
+      scores: [
+        "5",
+        "3",
+        "1",
+        "3",
+        "3",
+        "4",
+        "4",
+        "2",
+        "4",
+        "5"
+      ]
+    }
+  
+  ];
+
+  module.exports = friends;
+  
